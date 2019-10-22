b26aa77b9f14211ebd8e466d8ce1c17f94baff33
Lucas samuel <lukassamuka88@gmail.com>
2019-10-21 21:15:11 -0300

    Application generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :contacts
   resources :items
   get 'home/index'
-  root 'home#index'
+  root 'contacts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
